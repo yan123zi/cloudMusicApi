ec86e64b6a36eb0ea8b08efb0e541a275daeb5c2
yan123zi <673343330@qq.com>
2019-11-07 10:47:03 +0800

    update

--- module/album.js
+++ module/album.js
@@ -45,4 +45,19 @@ router.get("/comments/:id",(req,res,next)=>{
         res.send(result);
     });
 });
+//新碟上架
+router.get("/new",(req,res,next)=>{
+    let data={
+        area:req.query.type||"ALL",// ALL,ZH,EA,KR,JP
+        offset:req.query.offset||0,
+        total:true,
+        limit:req.query.limit||35,
+        csrf_token:""
+    };
+    request("post","https://music.163.com/weapi/album/new",data,{ua:"pc",crypto:"weapi"}).then(data=>{
+        let result=JSON.parse(data.body.replace(/[\r\n]/g,""));
+        result.desc="newAlbums";
+        res.send(result);
+    });
+});
 module.exports=router;
