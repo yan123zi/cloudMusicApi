70ddf270dd449ba553f840d67858b8c2b9e934a5
yan123zi <673343330@qq.com>
2019-11-22 20:19:12 +0800

    添加静态页面

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path = require('path');
 const fs = require("fs");
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
 const app = express();
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
@@ -14,6 +13,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use(express.static("public"))
 // app.use(express.static(path.join(__dirname, 'public')));
 
 
